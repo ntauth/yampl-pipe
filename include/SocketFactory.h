fecf2ad21d93a565f11cf8bc12310813c9e69c6a
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-26 14:00:52 +0200

    Converted the plugin to the new plugin system

--- include/SocketFactory.h
+++ include/SocketFactory.h
@@ -3,25 +3,29 @@
 
 #include "yampl/ISocketFactory.h"
 
-namespace yampl{
-namespace pipe{
+namespace yampl
+{
+    namespace pipe
+    {
+	opaque_ptr HOOK_CreateObject(object_init_params*);
+        hook_exec_status HOOK_DestroyObject(opaque_ptr);
 
-class SocketFactory : public ISocketFactory{
-  public:
-    SocketFactory();
+        class SocketFactory : public ISocketFactory
+	{
+            public:
+                SocketFactory();
 
-    virtual ISocket *createClientSocket(Channel channel, Semantics semantics = COPY_DATA, void (*deallocator)(void *, void *) = defaultDeallocator, const std::string& name = DEFAULT_ID);
-    virtual ISocket *createServerSocket(Channel channel, Semantics semantics = COPY_DATA, void (*deallocator)(void *, void *) = defaultDeallocator);
+                virtual ISocket *createClientSocket(Channel channel, Semantics semantics = COPY_DATA, void (*deallocator)(void *, void *) = defaultDeallocator, const std::string& name = DEFAULT_ID);
+                virtual ISocket *createServerSocket(Channel channel, Semantics semantics = COPY_DATA, void (*deallocator)(void *, void *) = defaultDeallocator);
 
-  private:
-    SocketFactory(const SocketFactory &);
-    SocketFactory & operator=(const SocketFactory &);
-};
+           private:
+               SocketFactory(const SocketFactory &);
+               SocketFactory & operator=(const SocketFactory &);
+        };
 
-inline SocketFactory::SocketFactory(){
-}
+        inline SocketFactory::SocketFactory() {}
 
-}
+    }
 }
 
 #endif
--- src/PipeSocketFactory.cpp
+++ src/PipeSocketFactory.cpp
@@ -8,6 +8,19 @@ namespace yampl
 {
     namespace pipe
     {
+        /****************** HOOK_*Object callbacks **/
+        opaque_ptr HOOK_CreateObject(object_init_params* params)
+	{
+	    static SocketFactory _singleton;
+            return reinterpret_cast<opaque_ptr>(&_singleton);
+        }
+
+        hook_exec_status HOOK_DestroyObject(opaque_ptr obj)
+        {
+            return HOOK_STATUS_SUCCESS;
+        }
+
+        /*************************** SocketFactory **/
 
         ISocket *SocketFactory::createClientSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *), const std::string& name){
           return new ClientSocket(channel, semantics, deallocator, name);
