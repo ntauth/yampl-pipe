9e9c48c0404d7e518299b5e6e09aadfba7d02ba5
Ayoub Chouak <a.chouak@protonmail.com>
2018-06-05 21:50:04 +0200

    Fixed incorrect plugin output directory

--- yampl-pipe.conf.cmake
+++ yampl-pipe.conf.cmake
@@ -11,7 +11,7 @@ if (NOT DEFINED WITH_PLUGIN_PIPE OR WITH_PLUGIN_PIPE)
 
     set_target_properties(yampl-pipe
             PROPERTIES
-            LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-shm"
+            LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-pipe"
     )
     target_compile_options(yampl-pipe PRIVATE "-Wno-terminate")
 
